2147c0c99c8f0cf9747dce576f7f1b065c1fa19a
varunyadav1175 <varunyadav1175@gmail.com>
2024-02-29 14:10:22 +0530

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,12 @@ import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
 
   return (
     <div>
-      <CreateTodo />
-      <Todos />
+      <CreateTodo></CreateTodo>
+      <Todos todos= {todos}></Todos>
     </div>
   )
 }
